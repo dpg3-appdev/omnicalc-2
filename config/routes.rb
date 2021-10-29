6fa8bdf3310c3014d00f274cc4196aece5787227
dpg3 <dguarin0@chicagobooth.edu>
2021-10-29 18:52:12 +0000

    Starting Omnicalc 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "perform_calc" })
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
